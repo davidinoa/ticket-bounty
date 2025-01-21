3e0ae91b3f9ba8cb8ce25a541572e58be0b20db1
David Inoa <davidinoa@gmail.com>
2025-01-20 20:08:20 -0400

    feat(lint): add simple-import-sort plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
-import prettier from 'eslint-config-prettier';
-import js from '@eslint/js';
 import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
+
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
@@ -14,6 +16,26 @@ export default ts.config(
 	...svelte.configs['flat/recommended'],
 	prettier,
 	...svelte.configs['flat/prettier'],
+	{
+		plugins: {
+			'simple-import-sort': simpleImportSort
+		},
+		rules: {
+			'simple-import-sort/imports': [
+				'error',
+				{
+					groups: [['^\\u0000', '^@?\\w', '^[^.]', '^\\.']]
+				}
+			],
+			'simple-import-sort/exports': 'error'
+		},
+		languageOptions: {
+			parserOptions: {
+				sourceType: 'module',
+				ecmaVersion: 'latest'
+			}
+		}
+	},
 	{
 		languageOptions: {
 			globals: {
