38f75da32aa05c235e1562ecfc4d1ec23199f439
David Inoa <davidinoa@gmail.com>
2025-01-25 12:39:43 -0400

    feat(theme): enhance dark mode and tailwind configuration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,13 +11,13 @@
 </svelte:head>
 
 <nav
-	class="supports-backdrop-blur:bg-background/60 bg-background/95 fixed left-0 right-0 top-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur"
+	class="border-border/40 bg-background/80 supports-[backdrop-filter]:bg-background/60 fixed left-0 right-0 top-0 z-20 flex w-full justify-between border-b px-5 py-2.5 backdrop-blur-sm"
 >
 	<a href={paths.home()} class="text-lg font-bold">Home</a>
 	<a href={paths.tickets.list()} class="text-sm underline">Tickets</a>
 </nav>
 <main
-	class="bg-secondary/20 flex min-h-screen flex-1 flex-col overflow-y-auto overflow-x-hidden px-8 py-24"
+	class="bg-background/20 flex min-h-screen flex-1 flex-col overflow-y-auto overflow-x-hidden px-8 py-24"
 >
 	{@render children()}
 </main>
